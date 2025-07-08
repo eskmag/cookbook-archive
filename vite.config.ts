46ed9d584fb94b84fcde37405a78a64121554a06
esmag0831 <esmag0831@uib.no>
2025-07-08 18:31:25 +0200

    fixed some 404 issues when refreshing

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
